50655d7a761c6964403b6ce80280beb90f8a08e6
Vivek Verma <vivekverma.imsec@gmail.com>
2024-07-16 13:04:12 +0530

    fixed compilation and bumped up the versions

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+// const nextConfig = {
+//     output: "standalone",
+// };
+//
+// export default nextConfig;
+
+module.exports = {
+    output: "standalone"
+}
